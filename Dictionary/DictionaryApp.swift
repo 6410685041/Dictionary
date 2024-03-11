e28328a3f794949463204c3b310f42ba1284e334
Chalisa Thummaraj <6410685041@student.tu.ac.th>
2024-03-11 14:49:19 +0700

    Initial Commit

---
+++ Dictionary/DictionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DictionaryApp.swift
+//  Dictionary
+//
+//  Created by ชลิศา ธรรมราช on 11/3/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct DictionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
